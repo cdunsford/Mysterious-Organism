423d0ac4848b8ce634dd2422724d215a1069a522
Christopher M Dunsford <42155296+cdunsford@users.noreply.github.com>
2020-10-30 17:10:14 -0400

    Add files via upload
    adding main.js file

---
+++ main.js
@@ -0,0 +1,116 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+    const dnaBases = ['A', 'T', 'C', 'G']
+    return dnaBases[Math.floor(Math.random() * 4)] 
+  }
+  
+  // Returns a random single stand of DNA containing 15 bases
+  const mockUpStrand = () => {
+    const newStrand = []
+    for (let i = 0; i < 15; i++) {
+      newStrand.push(returnRandBase())
+    }
+    return newStrand
+  }
+
+  const pAequorFactory =  (orgNumber, dnaBases) => {
+    
+    return {
+        specimenNum: orgNumber,
+        dna: dnaBases,
+        mutate: function () {
+
+            let randBase = '';
+            let randDNA = '';
+            let randNum = 0;
+
+            do {
+
+                randBase = returnRandBase();
+                randNum = Math.floor(Math.random() * 15);
+                randDNA = this.dna[randNum];
+
+
+            } while (randBase === randDNA);
+            
+            this.dna[randNum] = randBase;
+        },
+        compareDNA: function (pAeqor) {
+
+            let count = 0;
+            for (let index = 0; index < this.dna.length; index++) {
+                if(this.dna[index] === pAeqor.dna[index]) {
+                    count++;
+                }
+                
+            }
+            let percentDNAinCommon = ((count/15) * 100).toFixed(0);
+            
+            console.log(`specimen #${this.specimenNum} and specimen #${pAeqor.specimenNum} have ${percentDNAinCommon}% DNA in common`);
+        },
+        willLikelySurvive: function () {
+            
+            let count = 0;
+            for (let index = 0; index < this.dna.length; index++) {
+                if(this.dna[index] === 'C' || this.dna[index] === 'G') {
+                    count++;
+                }
+                
+            }
+            let percentToLikelySurvive = ((count/15) * 100).toFixed(0);
+
+            return percentToLikelySurvive >= 60 ? true : false;
+        }
+
+    };
+
+  };
+
+  const arrayOfpAequor = [];
+
+  for (let index = 0; index < 30; index++) {
+
+    arrayOfpAequor.push(pAequorFactory(index + 1,mockUpStrand()));
+    console.log(arrayOfpAequor[index].specimenNum + ':' + arrayOfpAequor[index].dna); 
+    if(arrayOfpAequor[index].willLikelySurvive())
+    {
+        console.log(`specimen #${arrayOfpAequor[index].specimenNum} is likely to survive`);
+
+    }
+    else {
+        console.log(`specimen #${arrayOfpAequor[index].specimenNum} is NOT likely to survive`);
+
+    }
+      
+      
+  }
+
+ 
+/* 
+ myOrganism.compareDNA(myOrganism2);
+
+if(myOrganism.willLikelySurvive())
+{
+     console.log(`specimen #${myOrganism.specimenNum} is likely to survive`);
+
+}
+else {
+    console.log(`specimen #${myOrganism.specimenNum} is NOT likely to survive`);
+
+}
+
+if(myOrganism2.willLikelySurvive())
+{
+     console.log(`specimen #${myOrganism2.specimenNum} is likely to survive`);
+
+}
+else {
+    console.log(`specimen #${myOrganism2.specimenNum} is NOT likely to survive`);
+
+}
+
+myOrganism.mutate();
+console.log(myOrganism.specimenNum + ':' + myOrganism.dna); 
+console.log(myOrganism2.specimenNum  + ':' + myOrganism2.dna); 
+myOrganism.compareDNA(myOrganism2);*/
+
